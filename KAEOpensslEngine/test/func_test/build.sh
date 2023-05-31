748be9ae49338e838808d4ef4ab5716f2eb99707
mayunhui <mayunhui3@huawei.com>
2023-05-31 16:24:53 +0800

    kae engine模块测试框架搭建
    Signed-off-by: mayunhui <mayunhui3@huawei.com>

--- KAEOpensslEngine/test/func_test/build.sh
+++ KAEOpensslEngine/test/func_test/build.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 BUILD_PATH=$(pwd)
-GOOGLE_TEST_DIR="${BUILD_PATH}/googletest-release-1.11.0"
-KAE_PATH="${BUILD_PATH}/../"
+gtest_download="${BUILD_PATH}/../gtest-download"
+GOOGLE_TEST_DIR="${gtest_download}/googletest-release-1.11.0"
 set -ex
 
 function build_googletest()
 {
-    mkdir -p test_tool_bins/gtest
+    mkdir -p "${BUILD_PATH}/test_tool_bins/gtest"
     mkdir -p "${GOOGLE_TEST_DIR}/build"
     cd "${GOOGLE_TEST_DIR}/build"
-    cmake ..
+    cmake ../
     make -j16
 
     cp ${GOOGLE_TEST_DIR}/build/lib/*.a ${BUILD_PATH}/test_tool_bins/gtest/
 }
 
-function build_kae()
-{
-    cd ${KAE_PATH}
-    sh build.sh all
-}
-
-function build_test()
-{
-    cd ${BUILD_PATH}/src
-    make -j96
-}
-
 function main()
 {
-    mkdir -p test_tool_bins
-    mkdir -p include
+    cd ${gtest_download}
+    sh download.sh
+    cd ${gtest_download}/googletest-release-1.11.0
     if [ ! -f "${BUILD_PATH}/test_tool_bins/gtest/libgtest.a" ]; then
         build_googletest
     fi
-    build_kae
     cd ${BUILD_PATH}/src
     sh build.sh
     make
