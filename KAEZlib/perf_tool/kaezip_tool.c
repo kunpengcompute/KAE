687c20fff3f5b65520994edc09b72aed1d99cbca
lauyarn <liuyang.nb@outlook.com>
2023-09-01 03:01:33 +0000

    添加多进程压缩性能测试函数
    Signed-off-by: lauyarn <liuyang.nb@outlook.com>

--- KAEZlib/perf_tool/kaezip_tool.c
+++ KAEZlib/perf_tool/kaezip_tool.c
@@ -15,7 +15,8 @@
 void usage(void)
 {
     printf("usage: \n");
-    // printf("  -m: multi process \n");
+    printf("  -m: multi process \n");
+    printf("  -t: multi thread \n");
     printf("  -s: stream length(KB)\n");
     printf("  -n: input data expanded n times.\n");
     printf("  -d: compress or uncompress, (0 for compress, 1 for uncompress) .\n");
@@ -74,14 +75,12 @@ uint8_t *get_compress_input(size_t input_sz)
 void do_perf_compress2(int level, unsigned char* output, uLong* output_sz, const unsigned char* inbuf, uLong stream_len)
 {
     int ret = 0;
-    int output_sz_bak = *output_sz;//因为每次压缩*output_sz值会变化，循环的时候需要初始化成一开始的情况。
     fflush(stdout);
     fflush(stderr);
 
     struct timeval start, stop;
     gettimeofday(&start, NULL);
 
-    *output_sz = output_sz_bak;
     ret = compress2(output, output_sz, inbuf, stream_len, level);
     if (ret != Z_OK) {
         printf("[KAE_ERR]:compress2 failed, ret is:%d.\n", ret);
@@ -169,6 +168,85 @@ int do_perf(const char* in_filename, uLong stream_len, int loop_times, int level
     free(inbuf);
     free(compressedData);
     free(decompressedData);
+    return 0;
+}
+
+int do_mul_perf(const char* in_filename, uLong stream_len, int loop_times, int level, int multi, int threadnum)
+{
+    if (multi <= 0 || threadnum <=0) {
+        printf("[ERROR]multi process cmd error.");
+        exit(-1);
+    }
+    int ret;
+    // 获取压缩数据：inbuf stream_len
+    void *inbuf = NULL;
+    if (in_filename) {
+        fprintf(stdout, "compress filename : %s\n", in_filename);
+        stream_len = read_inputFile(in_filename, &inbuf, loop_times);
+    } else {
+        stream_len = stream_len * loop_times;
+        inbuf = get_compress_input(stream_len);
+    }
+    if (!inbuf) {
+        fprintf(stderr, "inbuf is NULL!\n");
+        exit(-1);
+    }
+
+    pid_t pidChild = 0;
+    struct timeval start, stop;
+    for (int i = 0; i < multi; i++) {
+        pidChild = fork();
+        if (pidChild == 0) {
+            //子进程
+            break;
+        } else if (pidChild < 0) {
+            printf("%s fork failed\n", __func__);
+        }
+    }
+
+    if (pidChild > 0) {
+        // 父进程
+        printf("-------- Begin compress -------\n");
+        gettimeofday(&start, NULL);
+    }
+    
+    // 获取目的空间buf compressedData compressedSize
+    uLong blen = compressBound(stream_len);
+    uLong compressedSize = blen;
+    void *compressedData = malloc(compressedSize);
+    if (compressedData == NULL) {
+        exit(-1);
+    }
+    memset(compressedData, 0, compressedSize);
+                            
+    ret = compress2(compressedData, &compressedSize, inbuf, stream_len, level);
+    if (ret != Z_OK) {
+        printf("[KAE_ERR]:compress2 failed, ret is:%d.\n", ret);
+        exit(-1);
+    }
+
+    // 父进程等所有压缩任务结束
+    if (pidChild > 0) {
+        int ret = -1;
+        while (1) {
+            ret = wait(NULL);
+            if (ret == -1) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                break;
+            }
+        }
+
+        gettimeofday(&stop, NULL);
+        uint64_t time1 = (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec;
+        float speed1 = 1000000.0 / time1  * multi * stream_len / 1024 / 1024;
+
+        printf("kaezstd multi process perf result: [process num is %d, time used: %lu us, speed = %.3f MB/s]\n", multi, time1, speed1);
+    }
+
+    free(inbuf);
+    free(compressedData);
     return 0;
 }
 
@@ -289,21 +367,36 @@ int do_uncompress(const char* inputFile, const char* outputFile, int level)
     return 0;
 }
 
+#define OPT_MODE_PERF -1
+#define OPT_MODE_MULPERF -2
+#define OPT_MODE_COMPRESS 0
+#define OPT_MODE_DECOMPRESS 1
+
 int main(int argc, char **argv)
 {
     int opt = 0;
     int ret;
-    const char *optstring = "s:n:l:d:f:o:h";
+    const char *optstring = "m:t:s:n:l:d:f:o:h";
     // int multi = 2;
     uLong stream_len = 1024;
     int loop_times = 1;
     int level = 1;
     int perfFlag = -1; //初始值-1表示默认做perf计算，而不是作为压缩解压缩工具。
+    int processnum = 1;
+    int threadnum = 1;
     char input_filename[128] = {0};
     char output_filename[128] = {0};
     while ((opt = getopt(argc, argv, optstring)) != -1) {
         if(optstring == NULL) continue;
         switch (opt) {
+            case 'm':
+                processnum = atoi(optarg);
+                perfFlag = OPT_MODE_MULPERF;
+                break;
+            case 't':
+                threadnum = atoi(optarg);
+                perfFlag = OPT_MODE_MULPERF;
+                break;
             case 's':
                 stream_len = atoi(optarg);
                 break;
@@ -339,13 +432,16 @@ int main(int argc, char **argv)
 
 
     switch (perfFlag) {
-        case -1:
+        case OPT_MODE_PERF:
             ret = do_perf(in_filename, stream_len, loop_times, level);
             break;
-        case 0:
+        case OPT_MODE_MULPERF:
+            ret = do_mul_perf(in_filename, stream_len, loop_times, level, processnum, threadnum);
+            break;
+        case OPT_MODE_COMPRESS:
             ret = do_compress(in_filename, output_filename, level);
             break;
-        case 1:
+        case OPT_MODE_DECOMPRESS:
             ret = do_uncompress(in_filename, output_filename, level);
             break;
         default :
