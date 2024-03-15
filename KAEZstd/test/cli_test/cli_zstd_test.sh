7e0ef0f5592173add69a1e28ed4d95f8fe979027
lauyarn <liuyang.nb@outlook.com>
2024-03-15 10:44:36 +0000

    update perf script
    Signed-off-by: lauyarn <liuyang.nb@outlook.com>

--- KAEZstd/test/cli_test/cli_zstd_test.sh
+++ KAEZstd/test/cli_test/cli_zstd_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 # 测试文件路径
-test_file="test_file.txt"
-Decompression_file="test_file_decop.txt"
 RESFILE="res.txt"
 
 # zstd可执行文件路径
@@ -11,93 +8,62 @@ kaezstd_executable="/usr/local/kaezstd/bin/zstd"
 EXE="$zstd_executable $kaezstd_executable"
 
 # 压缩级别
-compression_level="1 2 3 4 5 6 7 8 9 10 15 20"
+compression_level="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 
-# 生成测试文件大小(M)
-test_file_size="1 10 20 30 40 50 100 500 1000"
-
-function generate_test_file() {
-    if [ $# -ne 1 ]; then
-        echo "Usage: generate_file <size_in_M>"
-        return 1
-    fi
-
-    size=$1
-    dd if=/dev/urandom of=$test_file bs=1M count=$size
-}
-
-function compare_files() {
-    if diff -q "$1" "$2" >/dev/null 2>&1; then
-        echo "TRUE"
-    else
-        echo "FALSE"
-    fi
-}
+# 测试文件名称
+# test_file_name="itemdata"
+BASE_TESTDATA_ADD="../../../scripts/compressTestDataset/"
+test_file_name="itemdata ooffice osdb samba webster xml x-ray"
 
 # 测试函数
 function run_test() {
-    local test_file="$1"
+    local filename="$1"
     local exe="$2"
     local compression_level="$3"
-    # local testfilesize="$4"
-    local conpress_time
-    local deconpress_time
-    local real_time
-    local user_time
-    local sys_time
-    local compress_rate
-    local COMPARE
-    local RES=""
+    # local win="$4"
+    local RESDATA
+    local COMPRESS_SPEED
+    local COMPRESS_RATE
+    local UNCOMPRESS_SPEED
+    local compressrate
 
-    RES="$exe , $compression_level , $(stat -c %s $test_file)"
+    echo "KAE_ZSTD_LEVEL=$compression_level $exe -b$compression_level $BASE_TESTDATA_ADD$filename"
+    RESDATA=$(KAE_ZSTD_LEVEL=$compression_level $exe -b$compression_level $BASE_TESTDATA_ADD$filename 2>&1 | tr -d '\r')
+    # RESDATA=$(KAE_ZSTD_LEVEL=$compression_level $exe -b$compression_level $BASE_TESTDATA_ADD$filename 2>&1)
+    
+    if [ $exe == $zstd_executable ]; then
+        COMPRESS_SPEED=$(echo $RESDATA | awk '{print $(59)}')
+        UNCOMPRESS_SPEED=$(echo $RESDATA | awk '{print $(62)}')
+        COMPRESS_RATE=$(echo $RESDATA | awk '{print $(58)}')
+    elif [ $exe == $kaezstd_executable ]; then
+        COMPRESS_SPEED=$(echo $RESDATA | rev | awk '{print $(5)}' | rev)
+        UNCOMPRESS_SPEED=$(echo $RESDATA | rev | awk '{print $(3)}' | rev)
+        COMPRESS_RATE=$(echo $RESDATA | rev | awk '{print $(6)}' | rev)
+    fi
 
-    # 压缩测试
-    TIMEFORMAT='%R %U %S'
-    conpress_time=$(time ($exe -$compression_level $test_file -o $test_file.zst >/dev/null ) 2>&1)
-    real_time=$(echo $conpress_time | awk '{print $(NF-2)}')
-    user_time=$(echo $conpress_time | awk '{print $(NF-1)}')
-    sys_time=$(echo $conpress_time | awk '{print $(NF-0)}')
+    # compressrate=$(echo "$COMPRESS_RATE" | awk -F'\[(x),]' '{print $2}')
+    compressrate=$(echo "$COMPRESS_RATE" | awk '{gsub(/[^0-9.]/, ""); print}')
 
-    RES="$RES , $(stat -c %s $test_file.zst) , $real_time , $user_time , $sys_time"
+    echo $exe $filename $compression_level $COMPRESS_SPEED $UNCOMPRESS_SPEED $compressrate >> $RESFILE
 
-    # printf "%lf %lf %lf" $real $user $sys >> $RESFILE
 
-    TIMEFORMAT='%R %U %S'
-    deconpress_time=$(time ($exe -d $test_file.zst -o $Decompression_file) 2>&1 >/dev/null)
-    real_time=$(echo $deconpress_time | awk '{print $(NF-2)}')
-    user_time=$(echo $deconpress_time | awk '{print $(NF-1)}')
-    sys_time=$(echo $deconpress_time | awk '{print $(NF-0)}')
-    RES="$RES , $real_time , $user_time , $sys_time"
-
-    COMPARE=`compare_files $test_file $Decompression_file`
-    RES="$RES , $COMPARE"
-    echo $RES >> $RESFILE
 }
 
-function clear(){
-    rm $test_file*
-    rm $Decompression_file
-}
 
 function main(){
-    clear
-    echo "可执行文件  ,  压缩等级  ,  压缩原始文件大小  ,  压缩后文件大小 , 压缩real_time ,  压缩user_time  , 压缩 sys_time ,  解压缩real_time ,  解压缩user_time  , 解压缩 sys_time ，一致性检查" > $RESFILE
-    
+    # clear
+    echo "EXE文件 , 压缩文件 , Level , CompressSpeed(MB/s) , UncompressSpeed(MB/s) , CompressRate" > $RESFILE
     for exe in $EXE
     do
-        for size in $test_file_size
+        for filename in $test_file_name
         do
             for level in $compression_level
             do
-                # 生成压缩文件
-                generate_test_file $size
-                # 测试压缩性能
-                run_test $test_file $exe $level $size
-                # 清除测试数据
-                clear
+                run_test $filename $exe $level
             done
         done
     done
+
 
 }
 
